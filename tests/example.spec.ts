5a687e02855e3fcc4e21ccacddd4606da4713a59
IvanK <ivan.kovalov@spsoft.com>
2023-04-07 15:02:42 +0300

    Update api login test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect, request } from '@playwright/test';
 import { App } from '../pages/App';
+import { Api } from '../pages/Api';
 
-test('Add Reading', async ({ page }) => {
+test.skip('Add Reading', async ({ page }) => {
   const app = new App(page)
   await page.goto('/login');
   await page.waitForLoadState('networkidle')
@@ -16,6 +17,18 @@ test('Add Reading', async ({ page }) => {
   await app.dashboardPage.selectReading()
 
 
+});
+
+
+
+test('Add api Reading', async ({ page }) => {
+  const apiContex = await request.newContext()
+  const api = new Api(apiContex)
+  const url = 'https://uat-dot-msuser-dot-dynamikax-dev.appspot.com/api/user/authenticate-with-user-name-no-captcha?compact=true',
+  emailAddress = "iagpmrole@yahoo.com",
+  password = "Second@123",
+  userName =  "iag_pm_automation"
+  await api.loginPage.loginUser(emailAddress, password, userName, url)
 });
 
 
